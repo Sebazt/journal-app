e321501ec3694f1830e1c3ecb9100c5a259f5cd8
Sebazt <juan.londono29@udea.edu.co>
2023-02-15 09:41:03 -0500

    :rocket: init  de project

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2">
+    <!-- title -->
+
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">08</span>
+      <span class="mx-1 fs-5">Febrero</span>
+      <span class="mx-1 fs-5 fw-light">2023</span>
+    </div>
+
+    <div class="entry-description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, porro? Officiis dignissimos, soluta aperiam pariatur rerum aliquid id sapiente iusto quos, vel obcaecati excepturi consequuntur, unde nesciunt adipisci qui modi.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+  }
+
+  .entry-container:hover{
+    background-color: rgba(128, 128, 128, 0.377);
+    transition: 0.2s all ease-in;
+
+  }
+
+  .entry-description{
+    font-size: 12px;
+  }
+</style>
---
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1 class="align-self-center">No have anything select</h1>
+  </div>
+
+  <fab/>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue';
+
+
+export default {
+  components:{
+    Fab: defineAsyncComponent( () => import('../components/Fab.vue'))
+  }
+};
+</script>
+
+
+<style scoped>
+div {
+  height: 100%;
+}
+</style>
